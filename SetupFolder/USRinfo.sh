9fc5eac7bafbe657111d1257aa10315b3748cfce
THE Unknown <lrco247@uky.edu>
2024-04-25 23:02:53 -0400

    test upload

--- SetupFolder/USRinfo.sh
+++ SetupFolder/USRinfo.sh
@@ -1,5 +1,4 @@
 export FABRIC_BASTION_LOGIN='lrco247_0000050326'
 export FABRIC_PROJECT_ID='7a5adb91-c4c0-4a1c-8021-7b6c56af196f'
 export FABRIC_BASTION_KEY="Fabric_Vagrant_VM_BASTION"
-export FABRIC_SLIVER_KEY="Fabric_Vagrant_VM"
 export FABRIC_TOKEN_NAME="id_token.json"
